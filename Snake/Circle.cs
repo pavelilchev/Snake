332d823ac028058ee9580a0dc62669270d3dbd42
Pavel Ilchev <pavel_ilchev@abv.bg>
2015-09-27 14:19:24 +0300

    add snake

---
+++ Snake/Circle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    public class Circle
+    {
+        public Circle()
+        {
+            this.X = 0;
+            this.Y = 0;
+        }
+
+        public int X { get; set; }
+
+        public int Y { get; set; }
+    }
+}
